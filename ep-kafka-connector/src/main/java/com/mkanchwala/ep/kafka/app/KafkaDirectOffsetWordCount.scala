82467721bd8f85c131576ea454c0f919e31f69a4
murtaza.kanchwala <murtuza.rajkahn@gmail.com>
2016-11-02 16:49:36 +0530

    New Dstream to DF updated

--- ep-kafka-connector/src/main/java/com/mkanchwala/ep/kafka/app/KafkaDirectOffsetWordCount.scala
+++ ep-kafka-connector/src/main/java/com/mkanchwala/ep/kafka/app/KafkaDirectOffsetWordCount.scala
@@ -5,6 +5,7 @@ import kafka.serializer.StringDecoder
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka._
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.SQLContext
 
 /**
  * Consumes messages from one or more topics in Kafka and does wordcount.
@@ -29,9 +30,10 @@ object KafkaDirectOffsetWordCount extends App {
 
   val Array(brokers, topics) = args
 
-  // Create context with 2 second batch interval
+  // Get StreamingContext from checkpoint data or create a new one
   val sparkConf = new SparkConf().setAppName("KafkaDirectOffsetWordCount").setMaster("local[4]")
   val ssc = new StreamingContext(sparkConf, Seconds(2))
+  ssc.checkpoint("/Kafka") // set checkpoint directory
 
   // Create direct kafka stream with brokers and topics
   val topicsSet = topics.split(",").toSet
@@ -47,18 +49,44 @@ object KafkaDirectOffsetWordCount extends App {
     rdd
   }.foreachRDD { rdd =>
     for (o <- offsetRanges) {
-      println(s"${o.topic} ${o.partition} ${o.fromOffset} ${o.untilOffset}")
-      
+      println(s" T=${o.topic};P=${o.partition};From=${o.fromOffset};Until=${o.untilOffset}")
+
     }
   }
-  
+
   // Get the lines, split them into words, count the words and print
   val lines = messages.map(_._2)
   val words = lines.flatMap(_.split(" "))
-  val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
-  wordCounts.print()
+  /* val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
+  wordCounts.print()*/
 
+  words.foreachRDD { rdd =>
+
+    // Get the singleton instance of SQLContext
+    val sqlContext = SQLContext.getOrCreate(rdd.sparkContext)
+    import sqlContext.implicits._
+
+    // Convert RDD[String] to DataFrame
+    val wordsDataFrame = rdd.toDF("word")
+
+    // Register as table
+    wordsDataFrame.registerTempTable("words")
+
+    // Do word count on DataFrame using SQL and print it
+    val wordCountsDataFrame =
+      sqlContext.sql("select word, count(*) as total from words group by word")
+    wordCountsDataFrame.show()
+  }
+  
   // Start the computation
   ssc.start()
   ssc.awaitTermination()
+
 }
+/*
+
+ T=DomainTest;P=0;From=1;Until=1
+ T=DomainTest;P=2;From=2;Until=2
+ T=DomainTest;P=1;From=2;Until=2
+  
+ */
